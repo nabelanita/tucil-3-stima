7004e05323455926c1d47612b95637ce71d2c817
Rafidika <rafidika2013@gmail.com>
2021-04-05 02:19:56 +0700

    a star done tp blom dites

--- src/Algoritma.py
+++ src/Algoritma.py
@@ -4,53 +4,71 @@ from Utility import *
 def AStar(simpulAwal, simpulTujuan, adjDict, listOfDistance):
     # adjDict adalah dictionary hasil return fungsi inputToAdj
     # listOfDistance adalah return fungsi inputToCoor
-    result = [simpulAwal]
-    tampunganSimpulTetangga = {}
+    result = []
+    tampunganSimpulTetangga = []
+    listSimpulTetangga = []
+    visited = {}
+    for key in listOfDistance:
+        visited[key] = False
+    evaluateSimpul(simpulAwal,simpulAwal,listSimpulTetangga,simpulTujuan,tampunganSimpulTetangga,result,visited,adjDict,listOfDistance)
 
-def updateTampunganSimpulTetangga(simpulAwal, simpulSaatIni, tampunganSimpulTetangga, adjDict):
-    if (len(tampunganSimpulTetangga) == 0):
-        for value in adjDict[simpulAwal]:
-            tampunganSimpulTetangga[value[0]] = list((simpulAwal))
+def evaluateSimpul(simpulAwal, simpulSekarang, listSimpulTetangga, simpulTujuan, tampunganSimpulTetangga, result, visited, adjDict, listOfDistance):
+    if (simpulSekarang == simpulTujuan):
+        listReturn = list(listSimpulTetangga)
+        listReturn.append(simpulSekarang)
+        return listReturn
     else:
-        for value in adjDict[simpulSaatIni]:
-            tampunganSimpulTetangga[value[0]] = 
+        listTampungan = list(listSimpulTetangga)
+        listTampungan.append(simpulSekarang)
+        # for dictionary in tampunganSimpulTetangga:
+        #     if simpulSekarang in dictionary:
+        #         listTampungan = list(dictionary[simpulSekarang])
+        #         listTampungan.append(simpulSekarang)
 
-def evaluateElements(simpulAwal, simpulSaatIni, simpulTujuan, adjDict, listOfDistance, tampunganSimpulTetangga):
-    # Assign tampunganSimpulTetangga dulu
-    
-    nilaiTerkecil = 0.0
-    simpulSelanjutnya = "placeholder"
-    for key in tampunganSimpulTetangga:
-        if (nilaiTerkecil == 0.0):
-            nilaiTerkecil = getJarakSimpulXKeSimpulAwal(tampunganSimpulTetangga,key,adjDict,simpulAwal) + getJarakLurusSimpulXKeSimpulTujuan(listOfDistance,key,simpulTujuan)
-            simpulSelanjutnya = key
-        else:
-            nilaiSaatIni = getJarakSimpulXKeSimpulAwal(tampunganSimpulTetangga,key,adjDict,simpulAwal) + getJarakLurusSimpulXKeSimpulTujuan(listOfDistance,key,simpulTujuan)
-            if (nilaiSaatIni < nilaiTerkecil):
-                nilaiTerkecil = nilaiSaatIni
-                simpulSelanjutnya = key
-    evaluateElements(simpulAwal,simpulSelanjutnya,simpulTujuan,adjDict,listOfDistance,tampunganSimpulTetangga)
-    
-def getJarakSimpulXKeSimpulAwal(tampunganSimpulTetangga, X, adjDict, simpulAwal):
-    # X adalah simpul yang ingin dicari jaraknya dari simpul awal
-    jarak = 0.0 # simpen nilai jarak
-    if (len(tampunganSimpulTetangga[X]) == 1): # cek kalo value tampunganSimpulTetangga (list) cuma punya satu elemen
-        listJarakDariSimpulAwal = adjDict[simpulAwal] # simpen list jarak semua simpul tetangga dari simpul awal
-        for value in listJarakDariSimpulAwal: # value bertipe tuple
-            if (value[0] == X): # kalo value index ke-0 dari tuple sama dengan X
-                jarak += value[1] # tambahin value[1] ke jarak
-    else: # value tampunganSimpulTetangga (list) punya lebih dari satu elemen
-        listPathSampaiX = tampunganSimpulTetangga[X] # simpen value dari tampunganSimpulTetangga[X] (list)
-        for i in range(len(listPathSampaiX)):
-            listJarak = adjDict[listPathSampaiX[i]] # tampung list jarak semua simpul tetangga dari listPathSampaiX[i]
-            if (i != len(listPathSampaiX)-1): # cek kalo i bukan index terakhir dari listPathSampaiX
-                for value in listJarak: # value bertipe tuple
-                    if (value[0] == listPathSampaiX[i+1]): # cek kalo value[0] sama dengan listPathSampaiX[i+1] 
-                        jarak += value[1]
-            else: # kalo i adalah index terakhir dari listPathSampaiX
-                for value in listJarak: # value bertipe tuple
-                    if (value[0] == X): # cek kalo value[0] sama dengan X
-                        jarak += value[1] 
+        for key in adjDict:
+            if (key == simpulSekarang):
+                for tup in adjDict[key]:
+                    if not (visited[tup[0]]):
+                        calonKey = tup[0]
+                        calonDict = {calonKey: listTampungan}
+                        tampunganSimpulTetangga.append(calonDict)
+        
+        for dictionary in tampunganSimpulTetangga:
+            if (simpulSekarang in dictionary):
+                if (isTwoListsEqual(listSimpulTetangga,dictionary[simpulSekarang])):
+                    visited[simpulSekarang] = True
+                    tampunganSimpulTetangga.remove(dictionary)
+                    break
+
+        nilaiTerkecil = 0.0
+        simpulWithNilaiTerkecil = "X"
+        for dictionary in tampunganSimpulTetangga:
+            for key in dictionary:
+                listSementara = list(dictionary[key])
+                listSementara.append(key)
+                jarakSementara = getJarakSimpulXKeSimpulAwal(listSementara,adjDict) + getJarakLurusSimpulXKeSimpulTujuan(listOfDistance,simpulSekarang,simpulTujuan)
+                if (nilaiTerkecil == 0.0):
+                    nilaiTerkecil = jarakSementara
+                    simpulWithNilaiTerkecil = key
+                else:
+                    if (jarakSementara < nilaiTerkecil):
+                        nilaiTerkecil = jarakSementara
+                        simpulWithNilaiTerkecil = key    
+
+        for dictionary in tampunganSimpulTetangga:
+            if simpulWithNilaiTerkecil in dictionary:
+                listTampungan = list(dictionary[simpulWithNilaiTerkecil])
+                # listTampungan.append(simpulSekarang)
+        evaluateSimpul(simpulAwal,simpulWithNilaiTerkecil,listTampungan,simpulTujuan,tampunganSimpulTetangga,result,visited,adjDict,listOfDistance)
+
+def getJarakSimpulXKeSimpulAwal(listUrutanJalan, adjDict):
+    jarak = 0.0
+    for i in range(len(listUrutanJalan)-1):
+        for key in adjDict:
+            if (key == i):
+                for tup in adjDict[key]:
+                    if (tup[0] == i+1):
+                        jarak += tup[1]
     return jarak
 
 def getJarakLurusSimpulXKeSimpulTujuan(listOfDistance, X, simpulTujuan):
@@ -62,3 +80,18 @@ def getJarakLurusSimpulXKeSimpulTujuan(listOfDistance, X, simpulTujuan):
         if (key == X):
             jarakLurus = dictJarakLurus[key]
     return jarakLurus
+
+def isTwoListsEqual(list1, list2):
+    equal = True
+    for element1 in list1:
+        if not (element1 in list2):
+            equal = False
+            break
+    if not (equal):
+        return equal
+    else:
+        for element2 in list2:
+            if not (element2 in list1):
+                equal = False
+                break
+        return equal
