62aafc7f132f5134db405baa4a0093ef67e80d92
nabelanita <utami.nabelanita@gmail.com>
2021-04-05 12:02:59 +0700

    more test files

--- src/Main.py
+++ src/Main.py
@@ -1,12 +1,13 @@
 from Algoritma import *
 
-r1 = inputToAdj("test1.txt")
-r2 = inputToCoor("test1.txt")
+r1 = inputToAdj("test3.txt")
+r2 = inputToCoor("test3.txt")
 
 
+print(r1)
 print(r2)
 
-distToRome = findDistanceTo(r2, 'Rome')
+# distToRome = findDistanceTo(r2, 'Rome')
 # print(distToRome)
 
-AStar('Rome', 'Madrid', r1, r2)
+# AStar('Rome', 'Madrid', r1, r2)
